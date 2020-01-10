39a466a1d7ab313a8931c0f557d613613213d0e3
Miroslav Boskovic (Freelancer) <miroslav.boskovic@delti.com>
2020-01-10 20:45:31 +0100

    first commit

---
+++ udacity-c3-restapi-user/src/config/config.ts
@@ -0,0 +1,22 @@
+export const config = {
+  "dev": {
+    "username": "udagram1",
+    "password": "udagram1",
+    "database": "udagram1",
+    "host": "udagram1.cdkpg8idgzad.us-east-2.rds.amazonaws.com",
+    "dialect": "postgres",
+    "aws_region": "us-east-2",
+    "aws_profile": "default",
+    "aws_media_bucket": "udagram-learn-bucket"
+  },
+  "prod": {
+    "username": "",
+    "password": "",
+    "database": "udagram_prod",
+    "host": "",
+    "dialect": "postgres"
+  },
+  "jwt": {
+    "secret" : "tajna"
+  }
+}
